fe7576bbf735edd37ab25a7d7e8c58e36521e24e
İbrahim Aksan <92032914+ibrahmaksan@users.noreply.github.com>
2023-12-23 00:08:19 +0300

    Update karekok-bulma.asm
    Yorum satırları eklendi

--- karekok-bulma.asm
+++ karekok-bulma.asm
@@ -1,43 +1,46 @@
 
-; Karekok Recursive
-org 900h            
-mov bx, 200H
+; IBRAHIM AKSAN 
+; Karekok Hesaplama (Alt Program Kullanarak)
+
+org 900h ; Ram' e 0100:0200 den itibaren yazmaktadir.      
+mov bx, 200H ; offseti 200h olarak aldim. 200h dan baslayarak yazacak.
         
 mov si, 0
-mov di,100          
+mov di,100 ; bu kadar donecek, 100 den baslayarak 0 a kadar tum sayilarin karekokunu hesaplayacak.        
 
-mov dx, 100        
-mov cl, 0
+mov dx, 100 ; sayi 100 den basladigi icin islem uzun surebilir. di ve dx i ayni sayi olmak
+;sartiyla daha kucuk bir sayi yazilirsa sonuc daha kisa surede hesaplanabilir.       
+mov cl, 0 ; sonucun tutuldugu register
           
 
 Tekrar: 
-    mov ax,0
-    mov dx,di
-    mov cx,0
-    call karekok
-    cmp di,0
-    jz son
-    jmp Tekrar          
+    mov ax,0 ; register sifirlandi.
+    mov dx,di ; yeni sayi dx e atildi. di benim dongu degiskenimdi.
+    mov cx,0 ; sonuc registeri sifirlandi.
+    call karekok ; karekok alt programini cagirdigim yer
+    cmp di,0; di = 0 olduysa artik program tamamlanmistir.
+    jz son   ; bitir
+    jmp Tekrar; bitmediyse yeni sayiyi alip devam edecek.    
 
 karekok proc             
     mov al, cl      
-    mul cl          
-    cmp ax, dx      
-    ja  Eksilt      
-    je  Bitir                  
-    inc cl          
-    jmp karekok       
+    mul cl ; ax = cl * cl oldu         
+    cmp ax, dx    
+    ja  Eksilt  ; ax , dx ' den buyukse sonucun bir azaltilmasi gerekir.      
+    je  Bitir   ; ax = dx ise sonuc cl dir ve direkt bitirilir.              
+    inc cl      ; ikisine de girmezse cl bir arttirilir.     
+    jmp karekok ; yeniden kontrol saglanir.      
 
 Eksilt:             
     dec cl          
-    jmp Bitir       
-
+           
 Bitir:              
-    mov [bx+si], cl
+    mov [bx+si], cl ; sonuc ram e yazilir.
     inc si
-    dec di 
-    ret
-    karekok endp
+    dec di ; sayi bir azaltilir.
+    karekok endp ;karekok alt programi sonlanir
+    ret ;karekok ' un cagrildigi yere doner.
+    
     
 Son:
     hlt             
